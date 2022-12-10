2882dbb2be596e02a73263da831a028f2babcb72
James Deane <jdeane@cloudflare.com>
2022-12-10 19:45:08 +1000

    emoji favicon

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ function createPage(config) {
   <head>
     <meta charset=utf-8>
     <title>${config.title}</title>
-    <link rel="favicon" href="https://r2.jwdn.cc/favicon.ico"/>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💰</text></svg>">
   </head>
   <body>
     <ul>
